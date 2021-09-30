05f482724d4a4398e3faedc9e7e8f67d67a2bf43
João Pedro Godinho <jpedrgod@everis.com>
2021-09-30 23:22:11 +0100

    review the consumer in order to create the queue when the process starts

--- rabbitmq-consumer/src/main/java/com/example/rabbitmqconsumer/configs/ConfigureRabbitMq.java
+++ rabbitmq-consumer/src/main/java/com/example/rabbitmqconsumer/configs/ConfigureRabbitMq.java
@@ -1,16 +1,40 @@
 package com.example.rabbitmqconsumer.configs;
 
 import com.example.rabbitmqconsumer.services.ConsumeMessageService;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.UUID;
+
 @Configuration
 public class ConfigureRabbitMq {
 
-    public static final String QUEUE_NAME = "hello";
+    public static final String QUEUE_NAME = "hello_"+UUID.randomUUID().toString();
+    public static final String EXCHANGE_NAME = "ex-hello";
+
+    @Bean
+    Queue createQueue() {
+        return new Queue(QUEUE_NAME, false);
+    }
+
+
+    @Bean
+    FanoutExchange exchange() {
+        return new FanoutExchange(EXCHANGE_NAME);
+    }
+
+
+    @Bean
+    Binding binding(Queue queue, FanoutExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange);
+    }
 
     @Bean
     SimpleMessageListenerContainer simpleMessageListenerContainer(ConnectionFactory connectionFactory, MessageListenerAdapter messageListenerAdapter) {
--- rabbitmq-producer/src/main/java/com/example/rabbitmqproducer/configs/ConfigureRabbitMq.java
+++ rabbitmq-producer/src/main/java/com/example/rabbitmqproducer/configs/ConfigureRabbitMq.java
@@ -6,26 +6,71 @@ import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class ConfigureRabbitMq {
 
-    public static final String QUEUE_NAME = "hello";
+    public static final String QUEUE_NAME = "publisher";
     public static final String EXCHANGE_NAME = "ex-hello";
 
+    /*
     @Bean
     Queue createQueue() {
         return new Queue(QUEUE_NAME, true);
     }
+
 
     @Bean
     FanoutExchange exchange() {
         return new FanoutExchange(EXCHANGE_NAME);
     }
 
+
     @Bean
     Binding binding(Queue queue, FanoutExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange);
     }
+
+    @Bean
+    List<Binding> bindings() {
+
+        List<String> lstQueues = new ArrayList<>();
+        lstQueues.add("hello_4c654958-5430-4b66-a9e5-67e73a3978f4");
+        lstQueues.add("hello_6d1f6e5b-8fb6-49c6-9096-60c9c4902917");
+
+        List<Binding> lstBidings = new ArrayList<>();
+
+        for(String queue : lstQueues)
+        {
+            lstBidings.add(BindingBuilder.bind(new Queue(queue)).to(exchange()));
+        }
+
+
+        return lstBidings;
+    }
+
+
+    @Bean
+    public Declarables fanoutBindings() {
+        Queue fanoutQueue1 = new Queue("hello_4c654958-5430-4b66-a9e5-67e73a3978f4", true);
+        Queue fanoutQueue2 = new Queue("hello_6d1f6e5b-8fb6-49c6-9096-60c9c4902917", true);
+        Queue fanoutQueue3 = new Queue("hello_947c5b75-fcec-4922-b1d3-0159a75b5117", true);
+
+        FanoutExchange fanoutExchange = new FanoutExchange(EXCHANGE_NAME);
+
+        return new Declarables(
+                fanoutQueue1,
+                fanoutQueue2,
+                fanoutQueue3,
+                fanoutExchange,
+                BindingBuilder.bind(fanoutQueue1).to(fanoutExchange),
+                BindingBuilder.bind(fanoutQueue2).to(fanoutExchange),
+                BindingBuilder.bind(fanoutQueue3).to(fanoutExchange)
+                );
+    }*/
 
     @Bean
     SimpleMessageListenerContainer container(ConnectionFactory connectionFactory) {
